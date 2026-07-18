6500f19e203c6e77d78789cd3007d0ade19003bc
Ash Harikrishnan <ashishharikrishnan@gmail.com>
2026-07-18 18:44:11 +0100

    Refactor Blob inputs to use named actions instead of hardcoded keys

--- entities/blob/character_body_2d.gd
+++ entities/blob/character_body_2d.gd
@@ -9,11 +9,11 @@ const JUMP_VELOCITY = -500.0
 var target:float=1.0
 
 func _process(delta: float) -> void:
-	if Input.is_key_pressed(KEY_W):
+	if Input.is_action_pressed("scale_up"):
 		target+=scale_speed*delta
-	if Input.is_key_pressed(KEY_S):
+	if Input.is_action_pressed("scale_down"):
 		target-=scale_speed*delta
-	
+
 	target= clamp(target,min_scale,max_scale)
 	scale = Vector2(target,target)
 func _physics_process(delta: float) -> void:
@@ -22,12 +22,11 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta * target
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("move_left", "move_right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
